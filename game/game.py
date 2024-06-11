50b58086229bc0888bd79cdee84f3b1fe42e9b40
Hovhannisyan111 <ogannisian.2004.01@gmail.com>
2024-06-11 12:40:49 +0400

    first

---
+++ game/game.py
@@ -0,0 +1,133 @@
+"""
+This file is for new game: pygame
+Created by: Arman Hovhannisyan
+Date: 29 May
+"""
+import time
+import random
+import pygame
+
+pygame.init()
+
+width = 800
+height = 600
+FPS = 60
+
+star_width = 7
+star_height = 30
+star_speed = 5
+star_rate = 0.03
+increase_rate = 0.1
+DINO_SPEED = 5
+
+try:
+    background = pygame.transform.scale(pygame.image.load("back.jpg"), (width, height))
+    dino_right = pygame.image.load("dinor.png")
+    dino_left = pygame.image.load("dinol.png")
+except FileNotFoundError:
+    print("Error loading image")
+    pygame.quit()
+    exit()
+
+font = pygame.font.SysFont("roman", 30)
+
+def draw(player, direction, finish, stars, window):
+    """
+    Function: draw
+    Brief: Draws the game in screen
+    Params: player:pygame.rect
+            direction: the direction of the player
+            finish: the time
+            stars: the list of falling stars
+            window: the game window
+    """
+    window.blit(background, (0, 0))
+    time_text = font.render(f"Time: {round(finish)}", 1, "white")
+    window.blit(time_text, (10, 10))
+
+    if direction == "right":
+        window.blit(dino_right, (player.x, player.y))
+    else:
+        window.blit(dino_left, (player.x, player.y))
+
+    for star in stars:
+        pygame.draw.rect(window, "white", star)
+
+    pygame.display.update()
+
+def star_fall(stars):
+    """
+    Function: star_fall
+    Brief: Generates stars random times
+    Params: stars: the list of stars
+    """
+    if random.random() < star_rate:
+        star_x = random.randint(0, width - star_width)
+        stars.append(pygame.Rect(star_x, 0, star_width, star_height))
+
+def game_over(stars, player, window):
+    """
+    Function: game_over
+    Brief: Checks it stars hit and removes stars
+    Params: stars: the list of stars, player: pygamr.rect, window: the game window
+    Retrun: true or false
+    """
+    for star in stars[:]:
+        if star.colliderect(player):
+            lose_text = font.render("You lose", 1, "red")
+            window.blit(lose_text, (width // 2 - lose_text.get_width() // 2,
+                height // 2 - lose_text.get_height() // 2))
+            pygame.display.update()
+            pygame.time.delay(2000)
+            return True
+        if star.y > height:
+            stars.remove(star)
+    return False
+
+def main():
+    """
+    Function: main
+    Entry point for the game.
+    """
+    window = pygame.display.set_mode((width, height))
+    pygame.display.set_caption("Dino")
+
+    run = True
+    player = pygame.Rect(200, height - dino_right.get_height(),
+            dino_right.get_width(), dino_right.get_height())
+    clock = pygame.time.Clock()
+    start = time.time()
+    stars = []
+    direction = "right"
+
+    while run:
+        clock.tick(FPS)
+        finish = time.time() - start
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+        key = pygame.key.get_pressed()
+        if key[pygame.K_RIGHT] and player.right < width:
+            player.move_ip(DINO_SPEED, 0)
+            direction = "right"
+        elif key[pygame.K_LEFT] and player.left > 0:
+            player.move_ip(-DINO_SPEED, 0)
+            direction = "left"
+
+        star_fall(stars)
+
+        star_v = star_speed + finish * increase_rate
+        for star in stars:
+            star.y += star_v
+
+        if game_over(stars, player, window):
+            run = False
+
+        draw(player, direction, finish, stars, window)
+
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
---
+++ speech/speech.py
@@ -0,0 +1,164 @@
+"""
+This file is for speech recognition
+Created by: Arman Hovhannisyan
+Date: 5 June
+"""
+import subprocess
+import os
+#import sys
+import webbrowser
+import speech_recognition as sr
+
+def open_firefox():
+    """
+    Function: open_firefox
+    Brief: opening firefox browser
+    """
+    try:
+        subprocess.run(["firefox"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        subprocess.Popen(["firefox"])
+    except FileNotFoundError:
+        print("There is no firefox or not in path")
+        exit()
+
+def search_firefox(search_term):
+    """
+    Function: search_firefox
+    Brief: searching what I say in firefox
+    Params: search_term: what I said
+    """
+    url = f"https://www.google.com/search?q={search_term.replace(' ', '+')}"
+    webbrowser.open(url)
+
+def create_file(file_name):
+    """
+    Function: create_file
+    Brief: creates a file
+    Params: file_name: name of the file
+    """
+    try:
+        with open(file_name, "w") as file:
+            file.write("")
+        print(f"File {file_name} created")
+    except FileExistsError:
+        print(f"File {file_name} already exists")
+        exit()
+
+def open_file(file_name):
+    """
+    Function: open_file
+    Brief: opening file
+    Params: file_name: name of the file
+    """
+    try:
+        subprocess.Popen(["xdg-open", file_name])
+    except FileNotFoundError:
+        print(f"Can't open {file_name}")
+        exit()
+
+def read_file(file_name):
+    """
+    Function: read_file
+    Brief: reads a file and prints its contents
+    Params: file_name: name of the file to read
+    """
+    try:
+        with open(file_name) as file:
+            content = file.read()
+            print(f"Contents of {file_name}")
+            print(content)
+    except FileNotFoundError:
+        print(f"File {file_name} doesn't exist")
+        exit()
+
+def delete_file(file_name):
+    """
+    Function: delete_file
+    Brief: deletes a file
+    Params: file_name: the name of the file to delete
+    """
+    try:
+        os.remove(file_name)
+        print(f"File {file_name} deleted")
+    except FileNotFoundError:
+        print(f"File {file_name} doesn't exist")
+        exit()
+
+def create_dir(directory):
+    """
+    Function: create_dir
+    Brief: creates a directory
+    Params: directory: the path of the directory to create
+    """
+    try:
+        os.makedirs(directory)
+        print(f"Directory {directory} created")
+    except FileExistsError:
+        print(f"Directory {directory} already exists")
+        exit()
+
+def list_dir(directory):
+    """
+    Function: list_dir
+    Brief: lists directory content
+    Params: directory: what directory I said
+    """
+    try:
+        files = os.listdir(directory)
+        if files:
+            print("Content of directory:")
+            for file in files:
+                print(file)
+        else:
+            print("Directory is empty")
+    except FileNotFoundError:
+        print(f"There is no {directory} directory")
+        exit()
+
+def main():
+    """
+    Function: main
+    Brief: entry point
+    """
+    recognizer = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        recognizer.adjust_for_ambient_noise(source)
+        audio = recognizer.listen(source)
+
+    try:
+        print("Recognizing speech...")
+        command = recognizer.recognize_google(audio).lower()
+        print("You said: ", command)
+        if "open firefox" in command:
+            open_firefox()
+        elif "search for" in command:
+            search_term = command.split("search for ")[-1]
+            search_firefox(search_term)
+        elif "create file" in command:
+            file_name = command.split("create file ")[-1]
+            create_file(file_name)
+        elif "open file" in command:
+            file_name = command.split("open file ")[-1]
+            open_file(file_name)
+        elif "read file" in command:
+            file_name = command.split("read file ")[-1]
+            read_file(file_name)
+        elif "delete file" in command:
+            file_name = command.split("delete file ")[-1]
+            delete_file(file_name)
+        elif "create directory" in command:
+            directory = command.split("create directory ")[-1]
+            create_dir(directory)
+        elif "list directory" in command:
+            directory = command.split("list directory ")[-1]
+            list_dir(directory)
+        else:
+            print("I don't understand the command")
+    except sr.RequestError:
+        print("Couldn't request results for speech recognition")
+    except sr.UnknownValueError:
+        print("Could not understand the audio")
+
+if __name__ == "__main__":
+    main()
